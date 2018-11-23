a6189f639a6cdb2fa87ed4c6dbdffd5400151dc0
Jobin <jobin.jia@beefinx.com>
2018-11-23 11:14:22 +0800

    'fix:bugs'

--- src/controllers/article.js
+++ src/controllers/article.js
@@ -1,24 +1,25 @@
 import Article from '../models/article'
 import Sequelize from 'sequelize'
+
 const Op = Sequelize.Op
 
 // 新增一篇文章
 export let addArticle = async (ctx) => {
-  let body = ctx.request.body
-  let { dataValues } = await Article.create({
-    title: body.title,
-    content: body.content
-  })
-  if (dataValues) {
+  try {
+    let body = ctx.request.body
+    let { dataValues } = await Article.create({
+      title: body.title,
+      content: body.content
+    })
     ctx.body = {
       result: dataValues,
       name: ctx.params.name,
       msg: '成功',
       code: 200
     }
-  } else {
+  } catch (e) {
     ctx.body = {
-      result: 'fail',
+      result: e,
       name: ctx.params.name,
       msg: '保存数据失败',
       code: 500
@@ -28,25 +29,25 @@ export let addArticle = async (ctx) => {
 
 // 逻辑删除文章
 export let delArticle = async (ctx) => {
-  let { id } = ctx.request.body
-  let res = await Article.update({
-    del: 1
-  }, {
-    where: {
-      id: id,
-      del: 0
-    }
-  })
-  if (res[0] === 1) {
+  try {
+    let { id } = ctx.request.body
+    let res = await Article.update({
+      del: 1
+    }, {
+      where: {
+        id: id,
+        del: 0
+      }
+    })
     ctx.body = {
       result: res,
       name: ctx.params.name,
       msg: '删除成功',
       code: 200
     }
-  } else {
+  } catch (e) {
     ctx.body = {
-      result: res,
+      result: e,
       name: ctx.params.name,
       msg: '删除失败',
       code: 500
@@ -56,31 +57,31 @@ export let delArticle = async (ctx) => {
 
 // 更新文章
 export let updArticle = async (ctx) => {
-  let { id, title, content, tags, cate } = ctx.request.body
-  let res = await Article.update(
-    {
-      title: title,
-      content: content,
-      tagId: tags,
-      cateId: cate
-    },
-    {
-      where: {
-        id: id,
-        del: 0
+  try {
+    let { id, title, content, tags, cate } = ctx.request.body
+    let res = await Article.update(
+      {
+        title: title,
+        content: content,
+        tagId: tags,
+        cateId: cate
+      },
+      {
+        where: {
+          id: id,
+          del: 0
+        }
       }
-    }
-  )
-  if (res.length !== 0) {
+    )
     ctx.body = {
       result: res,
       name: ctx.params.name,
       msg: '更新成功',
       code: 200
     }
-  } else {
+  } catch (e) {
     ctx.body = {
-      result: res,
+      result: e,
       name: ctx.params.name,
       msg: '更新失败',
       code: 500
@@ -90,17 +91,31 @@ export let updArticle = async (ctx) => {
 
 // 获取所有文章
 export let getArticle = async (ctx) => {
-  let { pageNo, pageSize, title } = ctx.query
-  let res = await Article.findAndCountAll({
-    where: {
-      del: 0,
+  try {
+    let { pageNo, pageSize, title } = ctx.query
+    let res = await Article.findAndCountAll({
       offset: (pageNo - 1) * pageSize,
-      limit: pageSize,
-      title: {
-        [Op.like]: `%${title}%`
+      limit: Number(pageSize)
+    }, {
+      where: {
+        del: 0,
+        title: {
+          [Op.like]: `%${title}%`
+        }
       }
+    })
+    ctx.body = {
+      result: res,
+      name: ctx.params.name,
+      msg: '更新失败',
+      code: 500
     }
-  })
-  console.log(res)
-  debugger
+  } catch (e) {
+    ctx.body = {
+      result: e,
+      name: ctx.params.name,
+      msg: '更新失败',
+      code: 500
+    }
+  }
 }
